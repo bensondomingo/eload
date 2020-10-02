7304893827e9bef4fa4b3282bccf8727cb75def5
Benson Domingo <benson.domingo@outlook.com>
2020-10-02 08:48:56 +0800

    sync frontend changes

---
+++ static/precache-manifest.c20563a79f84a5a4b13d6b4f49112291.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b9d3cb694a9eb0d6e0",
+    "url": "/static/bundle.css"
+  },
+  {
+    "revision": "f0b9d3cb694a9eb0d6e0",
+    "url": "/static/bundle.js"
+  },
+  {
+    "revision": "7fd5368f902c378fe544",
+    "url": "/static/dashboard.css"
+  },
+  {
+    "revision": "31d47085569e772c0f57aa8ec8381a5c",
+    "url": "/static/fonts/materialdesignicons-webfont.31d47085.woff"
+  },
+  {
+    "revision": "4a837d054b5f2a37170df8a275a13816",
+    "url": "/static/fonts/materialdesignicons-webfont.4a837d05.eot"
+  },
+  {
+    "revision": "b0fd91bb29dcb296a9a37f8bda0a2d85",
+    "url": "/static/fonts/materialdesignicons-webfont.b0fd91bb.ttf"
+  },
+  {
+    "revision": "f1997a8aba8a498fe4032e3b56e871ca",
+    "url": "/static/fonts/materialdesignicons-webfont.f1997a8a.woff2"
+  },
+  {
+    "revision": "c1aeee2f58af69aae662",
+    "url": "/static/home.css"
+  },
+  {
+    "revision": "d1582c9fd5eb962135df35cf7220431d",
+    "url": "/static/index.html"
+  },
+  {
+    "revision": "7fd5368f902c378fe544",
+    "url": "/static/js/dashboard.698e5875.js"
+  },
+  {
+    "revision": "c1aeee2f58af69aae662",
+    "url": "/static/js/home.b46255f6.js"
+  },
+  {
+    "revision": "ad25dab34b8190c3d531",
+    "url": "/static/js/login.388ee5fc.js"
+  },
+  {
+    "revision": "858444b9f27b18233be0",
+    "url": "/static/js/notfound.76472d32.js"
+  },
+  {
+    "revision": "3a3a78d3df82bbaa3470",
+    "url": "/static/js/profile.b35175b5.js"
+  },
+  {
+    "revision": "ad25dab34b8190c3d531",
+    "url": "/static/login.css"
+  },
+  {
+    "revision": "b05f3213f92c4eb17551bdb233c11164",
+    "url": "/static/manifest.json"
+  }
+]);
