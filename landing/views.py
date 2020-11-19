63a5c4d1e4753bb17cb1a95bce76e7804e0bd75b
Benson Domingo <benson.domingo@outlook.com>
2020-11-20 00:46:22 +0800

    fix leave commented code

--- landing/views.py
+++ landing/views.py
@@ -7,6 +7,6 @@ class IndexTemplateView(TemplateView):
     template_name = 'index.html'
 
     def get_template_names(self):
-        # if settings.DEBUG:
-        #     self.template_name = f'dev/{self.template_name}'
+        if settings.DEBUG:
+            self.template_name = f'dev/{self.template_name}'
         return super().get_template_names()
